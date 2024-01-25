a8769ece81fb0feeb2c2e0948f07a97d24206e98
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-26 10:48:58 +1100

    Bump aio_geojson_client to 0.20 (#30)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ DESCRIPTION = "An async GeoJSON client library for NSW Rural Fire Service Incide
 URL = "https://github.com/exxamalte/python-aio-geojson-nsw-rfs-incidents"
 
 REQUIRES = [
-    "aio_geojson_client>=0.18",
+    "aio_geojson_client>=0.20",
     "aiohttp>=3.7.4,<4",
     "pytz>=2019.01",
 ]
