2193df1f0a63bc7fa3d7af740a46067d9683b364
Malte Franken <exxamalte@users.noreply.github.com>
2024-10-07 00:45:12 +1100

    Migration to pyproject.toml (#37)

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Test for the NSW Rural Fire Service Incidents GeoJSON general setup."""
-
-from aio_geojson_nsw_rfs_incidents import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
